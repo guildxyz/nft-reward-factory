c0561c30fbf666846545a122039564db5456e768
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-08-02 22:04:52 +0200

    Update nft deploy script

--- scripts/deploy-nft.ts
+++ scripts/deploy-nft.ts
@@ -7,21 +7,23 @@ const name = ""; // The name of the token.
 const symbol = ""; // The short, usually all caps symbol of the token.
 const cid = ""; // The cid that will be returned by the tokenURI.
 const tokenOwner = "0x..."; /// The address that will be the owner of the deployed token.
+const treasury = "0x..."; // The address that will receive the price paid for mints.
+const tokenFee = 0; // The price of every mint in wei.
 const factoryAddress = "0x..."; /// The address of the proxy to be used when interacting with the factory.
 
 async function main() {
-  const GuildRewardNFT = await ethers.getContractFactory("GuildRewardNFT");
-  const guildRewardNFT = (await GuildRewardNFT.deploy()) as Contract;
+  const BasicGuildRewardNFT = await ethers.getContractFactory("BasicGuildRewardNFT");
+  const basicGuildRewardNFT = (await BasicGuildRewardNFT.deploy()) as Contract;
 
   const network = await ethers.provider.getNetwork();
   console.log(`Deploying contract to ${network.name !== "unknown" ? network.name : network.chainId}...`);
-  console.log(`Tx hash: ${guildRewardNFT.deploymentTransaction()?.hash}`);
+  console.log(`Tx hash: ${basicGuildRewardNFT.deploymentTransaction()?.hash}`);
 
-  await guildRewardNFT.waitForDeployment();
+  await basicGuildRewardNFT.waitForDeployment();
 
-  await guildRewardNFT.initialize(name, symbol, cid, tokenOwner, factoryAddress);
+  await basicGuildRewardNFT.initialize(name, symbol, cid, tokenOwner, treasury, tokenFee, factoryAddress);
 
-  console.log("GuildRewardNFT deployed to:", await guildRewardNFT.getAddress());
+  console.log("BasicGuildRewardNFT deployed to:", await basicGuildRewardNFT.getAddress());
 }
 
 main().catch((error) => {
