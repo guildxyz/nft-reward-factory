d856d6cde4780fa165c382ebdde4739ac36b9b47
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-11-15 16:20:23 +0100

    Implement EIP-5192

--- contracts/token/SoulboundERC721.sol
+++ contracts/token/SoulboundERC721.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 /* solhint-disable max-line-length */
 
+import { IERC5192 } from "../interfaces/IERC5192.sol";
 import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import { IERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import { ERC721EnumerableUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
@@ -14,7 +15,11 @@ import { IERC721EnumerableUpgradeable } from "@openzeppelin/contracts-upgradeabl
 /// @notice Allowance and transfer-related functions are disabled.
 /// @dev Inheriting from upgradeable contracts here - even though we're using it in a non-upgradeable way,
 /// we still want it to be initializable
-contract SoulboundERC721 is ERC721Upgradeable, ERC721EnumerableUpgradeable {
+contract SoulboundERC721 is ERC721Upgradeable, ERC721EnumerableUpgradeable, IERC5192 {
+    /// @notice Error thrown when trying to query info about a token that's not (yet) minted.
+    /// @param tokenId The queried id.
+    error NonExistentToken(uint256 tokenId);
+
     /// @notice Error thrown when a function's execution is not possible, because this is a soulbound NFT.
     error Soulbound();
 
@@ -28,7 +33,15 @@ contract SoulboundERC721 is ERC721Upgradeable, ERC721EnumerableUpgradeable {
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override(ERC721EnumerableUpgradeable, ERC721Upgradeable) returns (bool) {
-        return interfaceId == type(IERC721EnumerableUpgradeable).interfaceId || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(IERC5192).interfaceId ||
+            interfaceId == type(IERC721EnumerableUpgradeable).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function locked(uint256 tokenId) external view returns (bool) {
+        if (!_exists(tokenId)) revert NonExistentToken(tokenId);
+        return true;
     }
 
     function approve(
